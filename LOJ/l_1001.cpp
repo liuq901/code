fce92811570adf054930c340e0dd3f4705f0766b
Qi Liu <liuq901@163.com>
2014-03-21 20:58:31 +0800

    code

---
+++ LOJ/l_1001.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+int main()
+{
+    int T;
+    scanf("%d",&T);
+    while (T--)
+    {
+        int n;
+        scanf("%d",&n);
+        printf("0 %d\n",n);
+    }
+    return(0);
+}
+
