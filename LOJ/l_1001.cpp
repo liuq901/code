63e5f6676d0d356f783915a0dcd54b925889f46d
Qi Liu <liuq901@163.com>
2014-03-22 13:48:45 +0800

    code

--- LOJ/l_1001.cpp
+++ LOJ/l_1001.cpp
@@ -7,7 +7,10 @@ int main()
     {
         int n;
         scanf("%d",&n);
-        printf("0 %d\n",n);
+        if (n<10)
+            printf("0 %d\n",n);
+        else
+            printf("10 %d\n",n-10);
     }
     return(0);
 }
